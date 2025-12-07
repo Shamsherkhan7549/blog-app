dfe2ef90277e5d432908c4b97df74ea311a7cd67
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-12-07 08:09:54 -0800

    fronted setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import {BrowserRouter as Router,  Routes,Route} from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
+import About from './pages/About/About.jsx'
+import Contact from './pages/Contact/Contact.jsx'
+
+const App = () => {
+  return (
+    <Router>  
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </Router>
+
+  )
+}
+
+export default App
